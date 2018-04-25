e58c9bdc5fb6059e0f53942d02898a623d450082
Alexander Christner <christner.alex@gmail.com>
2018-04-24 22:27:41 -0400

    added cylinder object

---
+++ Assets/Suppot Scripts/CylinderSupport.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderSupport : MonoBehaviour, ISupport
+{
+    public float radius;
+    public float height;
+
+    public Vector3 Support(Vector3 direction)
+    {
+        direction = direction.normalized;
+
+        Vector3 w = direction - Vector3.Dot(transform.up, direction) * transform.up;
+
+        float sign = Mathf.Sin(Vector3.Dot(transform.up, direction)) > 0 ? 1 : -1;
+
+        Vector3 temp = transform.position + sign * height / 2 * transform.up;
+
+        if (w != Vector3.zero)
+        {
+            return temp + radius * w.normalized;
+        }
+        else
+        {
+            return temp;
+        }
+    }
+}
